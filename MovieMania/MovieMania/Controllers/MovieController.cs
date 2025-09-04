77021b857ad2b17d7b9fd56969b7908fc60a56ad
jgn98 <jonigadenielsen@gmail.com>
2025-09-04 11:29:21 +0200

    Add movie CRUD operations and validation
    Implemented Add, Edit, and Delete actions in MovieController. Added validation attributes to the Movie model. Created Add, Edit, and shared _Movie partial views for movie forms. Updated Index view to support editing and deleting movies.

--- MovieMania/MovieMania/Controllers/MovieController.cs
+++ MovieMania/MovieMania/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieMania.Models;
 using MovieMania.Persistence;
 
 namespace MovieMania.Controllers;
@@ -10,5 +11,48 @@ public class MovieController : Controller
     {
         var movies = MovieRepository.GetAll();
         return View(movies);
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Add(Movie movie)
+    {
+        if (ModelState.IsValid)
+        {
+            MovieRepository.Add(movie);
+            return RedirectToAction("Index");
+        }
+        return View(movie);
+    }
+    
+    [HttpGet]
+    public IActionResult Edit(int movieId)
+    {
+        var movie = MovieRepository.GetById(movieId);
+        
+            return View(movie);
+        
+    }
+    [HttpPost]
+    public IActionResult Edit(Movie movie)
+    {
+        if (ModelState.IsValid)
+        {
+            MovieRepository.Update(movie.MovieId ?? 0, movie);
+            return RedirectToAction("Index");
+        }
+        return View(movie);
+    }
+    
+    [HttpGet]
+    public IActionResult Delete(int movieId)
+    {
+        MovieRepository.Delete(movieId);
+        return RedirectToAction("Index");
     }
 }
--- MovieMania/MovieMania/Models/Movie.cs
+++ MovieMania/MovieMania/Models/Movie.cs
@@ -1,11 +1,27 @@
-﻿namespace MovieMania.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieMania.Models;
 
 public class Movie
 {
-    public int MovieId { get; set; }
+    [Key]
+    public int? MovieId { get; set; }
+    
+    [Required (ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name must be between 1 and 100 characters")]
     public string Title { get; set; }
+    
+    [Required]
+    [Range(1888, 2025, ErrorMessage = "Year must be between 1888 and 2025")]
     public int ReleaseYear { get; set; }
+    
+    [Required]
+    [Range(0, 1000, ErrorMessage = "Duration cannot be negative")]   
     public int DurationInMinutes { get; set; }
+    
+    [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters")]
     public string? Description { get; set; }
+    
+    [Range(1, 10, ErrorMessage = "Rating must be between 1 and 10")]  
     public int? Rating { get; set; }
 }
