7d62635a3aee4820f619fb139b397337ed2ca0a0
Jonas Gade <jonigadenielsen@gmail.com>
2025-11-16 21:11:40 +0100

    Add basic TCP client and server socket projects
    Introduced a simple TCP client and server implementation using sockets in .NET 8.0. The client connects to localhost on port 1234 and sends user input, while the server listens on the same port and prints received data. Added project files and solution for both components.

---
+++ 3. Semester/Tek11_Sockets/Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException: " + se.Message);
+                Main(args);
+            }
+            Console.WriteLine("Connected to server.");
+            string text = Console.ReadLine();
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(text);
+
+            sck.Send(data);
+            Console.WriteLine("Data sent to server.");
+            sck.Close();
+
+        }
+    }
+}
---
+++ 3. Semester/Tek11_Sockets/Tek11_Sockets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static byte [] Buffer { get; set; }
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(0, 1234));
+            sck.Listen(100);
+
+            Socket accepted = sck.Accept();
+            Buffer = new byte[accepted.SendBufferSize];
+            int receivedBytes = accepted.Receive(Buffer);
+            byte[] formatted = new byte[receivedBytes];
+            for (int i = 0; i < receivedBytes; i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            string strData = Encoding.ASCII.GetString(formatted);
+            Console.WriteLine("Received Data: " + strData);
+            accepted.Close();
+            sck.Close();
+        }
+    }
+}
